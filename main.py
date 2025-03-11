d836dccf4b0d8b8a6726dbfb6030bb785e96af63
mithrandir <adam010313@gmail.com>
2025-03-11 21:55:45 +0100

    plotting
    matplotlib and numpy added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("Hello Nándi")
 print("Hello Ádám!")
 print("Hello Valesz!")
+
+
+
+x = np.linspace(0,10,100)
+y = x**2
+
+plt.plot(x,y,"r")
